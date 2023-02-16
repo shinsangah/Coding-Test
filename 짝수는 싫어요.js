fa6ec47ec6d7b2d3a5f1ef07b9ecf9b50ed6931d
sang ah <kr.sangah@gmail.com>
2023-02-16 18:45:16 +0900

    230216

--- 짝수는 싫어요.js
+++ 짝수는 싫어요.js
@@ -1,47 +1,93 @@
-// 문제 설명
-// 정수 n이 매개변수로 주어질 때,
-// n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+// // 문제 설명
+// // 정수 n이 매개변수로 주어질 때,
+// // n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
 
-// 제한사항
-// 1 ≤ n ≤ 100
+// // 제한사항
+// // 1 ≤ n ≤ 100
 
-// 입출력 예 설명
+// // 입출력 예 설명
 
-// 입출력 #1
-// 10 이하의 홀수가 담긴 배열[1, 3, 5, 7, 9]를 return합니다.
+// // 입출력 #1
+// // 10 이하의 홀수가 담긴 배열[1, 3, 5, 7, 9]를 return합니다.
 
-// 입출력 #1
-// 15 이하의 홀수가 담긴 배열[1, 3, 5, 7, 9, 11, 13, 15]를 return합니다.
-
+// // 입출력 #1
+// // 15 이하의 홀수가 담긴 배열[1, 3, 5, 7, 9, 11, 13, 15]를 return합니다.
 
 // const tc1 = 3;
 // const tc2 = 2;
 // const tc3 = 15;
 
-// function solution(n) {
-//   let answer = []; // answer는 배열이 될거니까 [] 이렇게 시작
-
-//   for (let i = 1; i <= n; i++) { // 반복이니까 for문 씀
-//     if (i % 2 === 1) {
-//       answer.push(i); // 배열에 숫자 추가하는 것은 push(i)
-//     }
-//   }
-
-//   return answer;
-// }
-
-function solution(num) {
-  let answer = []; // answer는 배열이 될거니까 [] 이렇게 시작
-
-  for (let i = 1; i <= num; i += 2) { // 간단하게 tip 같은걸로 시간을 줄이는걸 알고리즘
-    answer.push(i);
-  }
-
-  return answer;
-}
-
 // console.log(solution(tc1));
 // console.log(solution(tc2));
 // console.log(solution(tc3));
 
-// 이렇게 하나 하나 외워나가면 됨
+// ↑ 맞게 출력되는지 확인하는 코드
+
+// 답 예시 1.
+function solution(n) {
+  let answer = []; // answer는 배열이 될거니까 [] 이렇게 시작
+
+  // 반복문이니까 for문 쓴 것
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 1) {
+      answer.push(i) // 배열에 숫자 추가하는 것임!
+    }
+  }
+  return answer;
+}
+
+// 답 예시 2.
+function solution(n) {
+  let answer = [];
+  for (let i = 1; i <= num; i += 2) { // 이런식으로 시간을 줄이는 알고리즘도 존재
+    answer.push(i)
+  }
+  return answer;
+}
+
+
+
+
+function solution(n) {
+  let answer = [];
+  for (let i = 1; i <= num; i += 2) {
+    answer.push(i)
+  }
+  return answer;
+}
+
+function solution(n) {
+  let answer = [];
+  for (i = 1; i <= n; i += 2); {
+    answer.push(i)
+  }
+  return answer;
+}
+
+function solution(n) {
+  let answer = [];
+  for (i = 1; i <= n; i += 2) {
+    answer.push(i)
+  }
+  return answer;
+}
+
+
+function solution(n) {
+  let answer = [];
+  for (i = 1; i <= n; i += 2) {
+    answer.push(i)
+  }
+  return answer;
+}
+
+
+function solution(n) {
+  let answer = [];
+  for (i = 1; i <= n, i++) {
+    if (i % 2 === 1) {
+      answer.push(i)
+    }
+  }
+  return answer;
+}
--- 짝수와 홀수.js
+++ 짝수와 홀수.js
@@ -1,6 +1,7 @@
 // 짝수와 홀수
 
 // 문제 설명
+
 // 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수,
 // solution을 완성해주세요.
 
@@ -18,22 +19,57 @@ const tc3 = 15;
 
 // 그냥 외우면 된다. 이게 짝수라고.
 
-function solution(num) {
-  let answer = "";
-  if (num % 2 === 0) {
-    answer = "Even"
-  } else {
-    answer = "odd"
-  }
-  return answer;
-  // return num % 2 === 0 ? "Even" : "Odd"
-}
-
 console.log(solution(tc1));
 console.log(solution(tc2));
 console.log(solution(tc3));
 
 // 짝수인지 홀수인지 확인하고,
 // 내 문제는 되는구나 하고 확인하고 솔루션 내부의 코드 copy 해서 프로그래머스에 붙이기
-// 프로그래머스 자체에서 하면 불편하니까 VS CODE 에서 하는 것을 추천하심
+// 프로그래머스 자체에서 하면 불편하니까 7VS CODE 에서 하는 것을 추천하심
 
+
+function solution(n) {
+  let answer = ""
+  if (n % 2 === 0) {
+    answer = "Even"
+  } else {
+    answer = "odd"
+  }
+  return answer;
+}
+
+function solution(n) {
+  let answer = "";
+  return n % 2 === 0 ? "Even" : "Odd"
+}
+
+function solution(n) {
+  let answer = "";
+  if (n % 2 === 0) {
+    answer = "Even"
+  } else {
+    answer = "Odd"
+  }
+  return answer;
+}
+
+function solution(n) {
+  let answer = "";
+  return n % 2 === 0 ? "Even" : "Odd"
+}
+
+
+fuction solution(n) {
+  let answer = "";
+  return n % 2 === 0 ? "Even" : "Odd"
+}
+
+function solutino(n) {
+  let answer = "";
+  if (n % 2 === 0) {
+    answer = "Even"
+  } else {
+    answer = "Odd"
+  }
+  return answer;
+}
