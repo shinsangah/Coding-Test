a4b7c1f018c1d3c1bc2f1fd2d0c6f70d3432b2fd
sang ah <kr.sangah@gmail.com>
2023-03-11 23:24:38 +0900

    230311

--- solution.js
+++ solution.js
@@ -1,3 +1,22 @@
+// 정수 num1과 num2가 주어질 때,
+// num1과 num2의 합을 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1. -50,000 <= num1 <= 50,000
+// 2. -50,000 <= num2 <= 50,000
+
+// 입출력 예
+// num1 num2 result
+// 2    3    5
+// 100  2    102
+
+// 입출력 예 설명
+// 입출력 예 #1
+// num1이 2이고 num2가 3이므로 2 + 3 = 5를 return 합니다.
+// 입출력 예 #2
+// num1이 100이고 num2가 2이므로 100 + 2 = 102를 return 합니다.
+
+
 function solution(num1, num2) {
   var answer = -1;
   return answer;
